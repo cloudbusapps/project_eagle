56f985565a2ad7d04cd98a8300129962088e2381
Shimsimi <hashim.mascara0605@gmail.com>
2022-12-20 13:49:21 +0800

    MERGE 2

---
+++ database/migrations/2022_12_20_133139_create_customer_assigned_consultants.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_assigned_consultants', function (Blueprint $table) {
+            $table->uuid('Id')->primary();
+            $table->uuid('CustomerId');
+            $table->string('UserId');
+            $table->uuid('CreatedById')->nullable();
+            $table->uuid('UpdatedById')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_assigned_consultants');
+    }
+};
