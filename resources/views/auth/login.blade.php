667106669c67fb6bc323dd6005abfbbb62511ac8
constARJAY <constarjay@gmail.com>
2023-02-13 19:04:06 +0800

    Development - Login UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,14 +34,14 @@
                                     <img src="{{ asset('assets/img/epldt-suite-logo.png') }}" height="50" width="50" alt="">
                                     <h4 class="px-2 font-weight-bolder d-none d-lg-block">ePLDT</h4>
                                 </a>
-                            </div><!-- End Logo -->
+                            </div>
 
                             <div class="card mb-3">
 
                                 <div class="card-body">
 
                                     <div class="pb-2">
-                                        <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                                        <h5 class="card-title text-center pb-0 fs-4 mb-0">PROJECT EAGLE</h5>
                                         <p class="text-center small">Enter your username & password to login</p>
                                     </div>
 
@@ -67,15 +67,22 @@
                                             <label for="email" class="form-label">Email Address</label>
                                             <div class="input-group has-validation">
                                                 <span class="input-group-text" id="inputGroupPrepend">@</span>
-                                                <input type="email" name="email" class="form-control"
-                                                    id="email" required value="{{ old('email') }}">
+                                                <input type="email" name="email" class="form-control" id="email" required value="{{ old('email') }}">
                                             </div>
                                         </div>
 
                                         <div class="col-12">
                                             <label for="password" class="form-label">Password</label>
-                                            <input type="password" name="password" class="form-control"
-                                                id="password" required>
+                                            
+
+                                            <div class="input-group has-validation">
+                                                <input type="password" name="password" class="form-control" id="password" required>
+                                                <div class="input-group-append">
+                                                    <button class="btn btn-outline-primary" type="button" id="btnPreviewPassword">
+                                                        <i class="bi bi-eye"></i>
+                                                    </button>
+                                                </div>
+                                            </div>
                                         </div>
 
                                         <div class="col-12">
@@ -85,13 +92,13 @@
                                                 <label class="form-check-label" for="rememberMe">Remember Me</label>
                                             </div>
                                         </div>
-                                        <div class="col-12">
-                                            <button class="btn btn-primary w-100" type="submit">Login</button>
+                                        <div class="col-12 mt-3">
+                                            <button class="btn btn-primary w-100" type="submit"><i class="bi bi-send-fill"></i> Log In</button>
                                         </div>
-                                        <div class="col-12">
+                                        {{-- <div class="col-12">
                                             <p class="small mb-0">Don't have account? <a
                                                     href="{{ route('auth.register') }}">Create an account</a></p>
-                                        </div>
+                                        </div> --}}
                                     </form>
 
                                 </div>
@@ -107,5 +114,25 @@
     </main>
 
 </body>
+
+<script>
+
+    $(document).ready(function() {
+        $(document).on('click', '#btnPreviewPassword', function(e) {
+            e.preventDefault(); 
+            let isPreview = $(this).attr('preview') ?? 'true';
+            if (isPreview == 'true') {
+                $(this).html(`<i class="bi bi-eye-slash"></i>`);
+            } else {
+                $(this).html(`<i class="bi bi-eye"></i>`);
+            }
+            $(`[name="password"]`).attr('type', isPreview == 'true' ? 'text' : 'password');
+
+            isPreview = isPreview == 'true' ? 'false' : 'true';
+            $(this).attr('preview', isPreview);
+        })
+    })
+
+</script>
 
 </html>
