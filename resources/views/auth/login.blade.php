60ea4751c9fea4d16ee0e9502ad82ada8dea9b23
constARJAY <constarjay@gmail.com>
2023-02-20 11:33:32 +0800

    Testing - Change title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>[PROD] ePLDT | Login</title>
+    <title>ePLDT | Login</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
